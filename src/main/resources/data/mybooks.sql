34586db4b9a4589200b69b8cd2a4b6f289531d5c
Tomasz Lisiecki <lisiecki.tomek@gmail.com>
2015-10-28 22:39:28 +0100

    change in mybooks.sql

--- src/main/resources/data/mybooks.sql
+++ src/main/resources/data/mybooks.sql
@@ -4,3 +4,4 @@ INSERT INTO mybooks VALUES (2,'World History',to_date('02-01-2002', 'dd-mm-yyyy'
 INSERT INTO mybooks VALUES (3,'Medicine History',to_date('03-01-2002', 'dd-mm-yyyy'));
 INSERT INTO mybooks VALUES (4,'Market History',to_date('04-01-2002', 'dd-mm-yyyy'));
 INSERT INTO mybooks VALUES (5,'Weather History',to_date('05-01-2002', 'dd-mm-yyyy'));
+INSERT INTO mybooks VALUES (6,'Dojpa',to_date('05-01-2002', 'dd-mm-yyyy'));
